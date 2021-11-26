5ea2f78e85481dff96e1bad8cca6a93be93cdfca
jwhodge <jonathan@jonathanhodge.co.nz>
2021-11-26 11:50:20 +0800

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-mdx",
+    "gatsby-plugin-netlify-cms",
     {
       resolve: "gatsby-source-filesystem",
       options: {
